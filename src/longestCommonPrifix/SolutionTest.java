9bf7894a21d6295d1927deb32b73fc69b042e5bc
Lee-Scott <37646873+Lee-Scott@users.noreply.github.com>
2020-07-13 17:07:30 -0400

    leetCode 5/14 easy listNode Sort

---
+++ src/longestCommonPrifix/SolutionTest.java
@@ -0,0 +1,21 @@
+package longestCommonPrifix;
+
+import longestCommonPrifix.Solution;
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class SolutionTest {
+
+    @Test
+    public void Test1() {
+        String arr[] = {"flower", "flow",
+                "flight"};
+
+        int n = arr.length;
+
+        Assert.assertEquals("fl", Solution.commonPrefix(arr, n));
+
+    }
+
+}
